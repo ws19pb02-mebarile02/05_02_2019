ea4334f65ff72b583844fe28d705c12a0ecfa3e3
Michael Barile <33465350+mebarile02@users.noreply.github.com>
2019-05-03 20:31:11 -0400

    consolidated nested for loops

--- checkerboard.py
+++ checkerboard.py
@@ -19,16 +19,10 @@ root.geometry(f"{board_width}x{board_height}")
 canvas = tkinter.Canvas(root, highlightthickness = 0, background = "white")
 
 for i in range(8):
-    if i%2 == 0:
-        for j in range(8):
-            if j%2 != 0:
-                canvas.create_rectangle(boxHeight*j,boxHeight*i,
-                    boxHeight*(j+1),boxHeight*(i+1), width = 0, fill = "black")
-    else:
-        for j in range(8):
-            if j%2 == 0:
-                canvas.create_rectangle(boxHeight*j,boxHeight*i,
-                boxHeight*(j+1),boxHeight*(i+1), width = 0, fill = "black")
+    for j in range(8):
+        if (i + j) % 2 != 0:
+            canvas.create_rectangle(boxHeight*j, boxHeight*i,
+                    boxHeight*(j+1), boxHeight*(i+1), width = 0, fill = "black")
 
 canvas.pack(expand = tkinter.YES, fill = "both")
 
