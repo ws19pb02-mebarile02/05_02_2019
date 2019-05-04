5c5dc2bf9216cdfb5d0f6286ad82b52c2645b8c0
Michael Barile <33465350+mebarile02@users.noreply.github.com>
2019-05-03 20:45:25 -0400

    spacing

--- itflag.py
+++ itflag.py
@@ -12,8 +12,8 @@ stripeWidth = 300
 root = tkinter.Tk()
 root.title("Italian Flag")
 
-width = 3*stripeWidth
-height = int(width*2/3)  # Aspect ratio 2:3 according to Wiki
+width = 3 * stripeWidth
+height = int(width * 2/3)  # Aspect ratio 2:3 according to Wiki
 root.geometry(f"{width}x{height}")
 
 """
@@ -22,9 +22,9 @@ Hex colors corresponding to official colors on Wiki
 
 canvas = tkinter.Canvas(root, highlightthickness = 0, background = "#F4F5F0")
 
-canvas.create_rectangle(0,0,stripeWidth,height, width = 0, fill = "#008C45")
+canvas.create_rectangle(0, 0, stripeWidth, height, width = 0, fill = "#008C45")
 
-canvas.create_rectangle(2/3*width, 0 , width, height, width = 0, fill = "#CD212A")
+canvas.create_rectangle(width * 2/3, 0 , width, height, width = 0, fill = "#CD212A")
 
 
 canvas.pack(expand = tkinter.YES, fill = "both")
